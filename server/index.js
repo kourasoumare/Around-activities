3960bde47b37315e245820d0f228fbc5e1da66db
Koura <kourasoumare113@gmail.com>
2026-05-17 17:36:37 +0000

    feat: configuration Prisma

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+/*preparation des routes pour mes devs*/
+import authRoutes from './routes/auth.js'
+import userRoutes from './routes/users.js'
+import activityRoutes from './routes/activities.js'
+import groupRoutes from './routes/groups.js'
 
 dotenv.config();
 
@@ -8,6 +13,15 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.json({ message: "Bienvenue sur l'API Around Activities" })
+})
+
+/*preparations des routes pour mes devs*/
+app.use('/api/auth', authRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/activities', activityRoutes)
+app.use('/api/groups', groupRoutes)
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Serveur démarré sur le port ${process.env.PORT || 5000}`);
 });
