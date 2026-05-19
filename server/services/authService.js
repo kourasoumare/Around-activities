4f2109c547fc00c720cc8bc208b9c6bcbe75ba12
bambi cisse <cissebambi451@gmail.com>
2026-05-19 20:05:35 +0200

    feat: ajout route GET /api/users/me/groups

--- server/services/authService.js
+++ server/services/authService.js
@@ -1,27 +1,29 @@
 import prisma from "../config/prisma.js"
 
-export const register = async ({prénom, Nom, Email, mot_de_passe, confirmer_le_mot_de_passe, ville}) => {
-  
-  // 400 - Mots de passe différents
+export const register = async ({
+  prénom,
+  Nom,
+  Email,
+  mot_de_passe,
+  confirmer_le_mot_de_passe,
+  ville
+}) => {
   if (mot_de_passe !== confirmer_le_mot_de_passe) {
     const error = new Error("Les mots de passe ne correspondent pas")
     error.statusCode = 400
     throw error
   }
 
-  // 409 - Email déjà utilisé
   const existsUser = await prisma.users.findUnique({
     where: { email: Email }
   })
 
   if (existsUser) {
-    const error = new Error("Cet email est déjà utilisé")
     const error = new Error("L'Email a déjà été utilisé")
     error.statusCode = 409
     throw error
   }
 
-  // Créer l'utilisateur
   const newUser = await prisma.users.create({
     data: {
       first_name: prénom,
@@ -33,34 +35,24 @@ export const register = async ({prénom, Nom, Email, mot_de_passe, confirmer_le_
   })
 
   const { password, ...userSansMotDePasse } = newUser
-  return { message: "Inscription réussie", user: userSansMotDePasse }
-  return { message: "Inscription réussie", user: newUser }
+
+  return {
+    message: "Inscription réussie",
+    user: userSansMotDePasse
+  }
 }
 
 export const login = async ({ Email, mot_de_passe }) => {
-
-  // 400 - Champs manquants
   if (!Email || !mot_de_passe) {
     const error = new Error("Email et mot de passe obligatoires")
     error.statusCode = 400
     throw error
   }
 
-  // 404 - Email introuvable
-  // Vérifier que l'email existe et que le mot de passe est correct
   const user = await prisma.users.findUnique({
     where: { email: Email }
   })
 
-  if (!user) {
-    const error = new Error("Aucun compte associé à cet email")
-    error.statusCode = 404
-    throw error
-  }
-
-  // 401 - Mauvais mot de passe
-  if (user.password !== mot_de_passe) {
-    const error = new Error("Mot de passe incorrect")
   if (!user || user.password !== mot_de_passe) {
     const error = new Error("Email ou mot de passe incorrect")
     error.statusCode = 401
@@ -68,8 +60,9 @@ export const login = async ({ Email, mot_de_passe }) => {
   }
 
   const { password, ...userSansMotDePasse } = user
-  return { message: "Connexion réussie", user: userSansMotDePasse }
+
+  return {
+    message: "Connexion réussie",
+    user: userSansMotDePasse
+  }
 }
-  // Retourner l'utilisateur connecté
-  const { password, ...userSansMotDePasse } = user
-return { message: "Connexion réussie", user: userSansMotDePasse }}
