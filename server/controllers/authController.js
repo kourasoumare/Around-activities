31699fd0ee71c45366d8fd4e6b03a244858a9b69
bambi cisse <cissebambi451@gmail.com>
2026-05-21 22:29:37 +0200

    feat: ajout forgot-password et reset-password

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -32,4 +32,36 @@ const login = async (req, res, next) => {
   }
 }
 
-export default { register, login }
+// Forgot password
+const forgotPassword = async (req, res, next) => {
+  try {
+    const { email } = req.body
+
+    if (!email) {
+      return res.status(400).json({ message: "Email is required" })
+    }
+
+    const result = await authService.forgotPassword({ email })
+    res.status(200).json(result)
+  } catch (error) {
+    next(error)
+  }
+}
+
+// Reset password
+const resetPassword = async (req, res, next) => {
+  try {
+    const { token, newPassword } = req.body
+
+    if (!token || !newPassword) {
+      return res.status(400).json({ message: "Token and new password are required" })
+    }
+
+    const result = await authService.resetPassword({ token, newPassword })
+    res.status(200).json(result)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export default { register, login, forgotPassword, resetPassword }
--- server/routes/groups.js
+++ server/routes/groups.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import { createGroup, joinGroup } from '../controllers/groupController.js'
-import { verifyToken } from '../middleware/auth.js'
+import { generateToken } from "../utils/token.js"
 
 const router = express.Router()
 
-router.post('/', verifyToken, createGroup)
-router.post('/:id/join', verifyToken, joinGroup)
+router.post('/', generateToken,createGroup)
+router.post('/:id/join', generateToken, joinGroup)
 
 export default router
