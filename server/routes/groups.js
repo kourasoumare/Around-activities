4c6f7b1ec5012c87313f74ca0bba4e1ed3b14b54
bambi cisse <cissebambi451@gmail.com>
2026-05-22 16:15:12 +0200

    rectification token

--- server/routes/groups.js
+++ server/routes/groups.js
@@ -1,10 +1,9 @@
 import express from 'express'
 import { createGroup, joinGroup } from '../controllers/groupController.js'
-import { generateToken } from "../utils/token.js"
-
+import { verifyToken } from '../utils/token.js'
 const router = express.Router()
 
-router.post('/', generateToken,createGroup)
-router.post('/:id/join', generateToken, joinGroup)
+router.post('/', verifyToken,createGroup)
+router.post('/:id/join', verifyToken,joinGroup)
 
 export default router
